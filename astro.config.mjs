1eabd4871ff3bc0015ecaa475505756466d5206a
zhengzhuang <zhengzhuang@yuanduyun.com>
2026-04-17 14:42:13 +0800

    feat: astro重构

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
+  site: 'https://zhengzhuangpro.pages.dev',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
